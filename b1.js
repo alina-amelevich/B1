cf4f6845ca217d7ecffee0cbbe77b9462b38763b
Alina <alina@Apples-MBP.lan>
2020-12-03 19:07:15 +0200

    сделала, чтобы век выводился для пользователя

--- b1.js
+++ b1.js
@@ -1,12 +1,10 @@
 'use strict'
 
-alert('Hello!');
-
 function calcCentury(year) {
     return (Math.floor((year/100) + 1));
 }
 
 function runB1() {
     let userYear = parseInt(prompt('Введите год целым числом'));
-    calcCentury(userYear);
+    alert(calcCentury(userYear) + 'век');
 }
