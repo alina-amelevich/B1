00609cad0b8307d976c0eb54743d533a5d41005b
Alina <alina@Apples-MBP.lan>
2020-12-03 18:58:58 +0200

    Написала чистую ф-цию и ее запуск

--- b1.js
+++ b1.js
@@ -1,3 +1,12 @@
 'use strict'
 
-alert("Hello!");
+alert('Hello!');
+
+function calcCentury(year) {
+    return (Math.floor((year/100) + 1));
+}
+
+function runB1() {
+    let userYear = parseInt(prompt('Введите год целым числом'));
+    calcCentury(userYear);
+}
