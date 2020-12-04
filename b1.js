5d66cb020263edf6df0596b85ff591fa2c38e224
Alina <alina@Apples-MBP.lan>
2020-12-05 01:23:23 +0200

    исправила простейшую обработку исключений

--- b1.js
+++ b1.js
@@ -4,12 +4,11 @@ function calcCentury(year) {
     return (Math.floor((year/100) + 1));
 }
 
-try {
-    function runB1() {
+function runB1() {
+    try {
         let userYear = parseInt(prompt('Введите год нашей эры целым числом'));
         alert(calcCentury(userYear) + 'век');
+    } catch(ex) {
+        alert('Что-то пошло не так, попробуйте еще раз!');
     }
-}
-catch(ex) {
-    alert('Что-то пошло не так, попробуйте еще раз!');
 }
