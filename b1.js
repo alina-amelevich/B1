6678d80388cd2369447686647dad8e5decbf0688
Alina <alina@Apples-MBP.lan>
2020-12-05 02:24:06 +0200

    Доб. проверку с пом. мат.выраж-я и обр-ку исключ-я что позволяет рассчитать век до н.э. с помощью вызова новой ф-ции alcCenturyPastEra

--- b1.js
+++ b1.js
@@ -3,11 +3,20 @@
 function calcCentury(year) {
     return (Math.floor((year/100) + 1));
 }
+function calcCenturyPastEra(year) {
+    return (Math.floor(year/100));
+}
 
 function runB1() {
     try {
-        let userYear = parseInt(prompt('Введите год нашей эры целым числом'));
+        let userYear = parseInt(prompt('Введите год целым числом'));
         alert(calcCentury(userYear) + 'век');
+        try {
+            (userYear / Math.abs(userYear) + 1) / 2
+        } catch(ex) {
+            alert(Math.abs(calcCenturyPastEra(userYear)) + 'век до нашей эры');
+            return;
+        }
     } catch(ex) {
         alert('Что-то пошло не так, попробуйте еще раз!');
     }
