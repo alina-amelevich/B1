210c9c4436b8b8b8b71a4469f069f8329ce7614c
Alina <alina@Apples-MBP.lan>
2020-12-05 02:45:41 +0200

    исправила математику

--- b1.js
+++ b1.js
@@ -11,7 +11,7 @@ function runB1() {
     try {
         let userYear = parseInt(prompt('Введите год целым числом'));
         try {
-            ((userYear / Math.abs(userYear) + 1) / 2) / 2;
+            1 / ((userYear / Math.abs(userYear) + 1) / 2);
             alert(calcCentury(userYear) + 'век');
         } catch(ex) {
             alert(Math.abs(calcCenturyPastEra(userYear)) + 'век до нашей эры');
