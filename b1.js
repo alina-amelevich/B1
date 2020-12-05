2621205f1d608956aa7ff1a339351aab76bcc8e6
Alina <alina@Apples-MBP.lan>
2020-12-05 03:05:42 +0200

    Исключение не срабатывало, т.к. деление на 0 не вызывает ошибку (что было в математической проверке). Забила. Вернула как было,  без рассчета до н.э.

--- b1.js
+++ b1.js
@@ -3,20 +3,11 @@
 function calcCentury(year) {
     return (Math.floor((year/100) + 1));
 }
-function calcCenturyPastEra(year) {
-    return (Math.ceil(year/100));
-}
 
 function runB1() {
     try {
-        let userYear = parseInt(prompt('Введите год целым числом'));
-        try {
-            1 / ((userYear / Math.abs(userYear) + 1) / 2);
-            alert(calcCentury(userYear) + 'век');
-        } catch(ex) {
-            alert(Math.abs(calcCenturyPastEra(userYear)) + 'век до нашей эры');
-            return;
-        }
+        let userYear = parseInt(prompt('Введите год нашей эры целым числом'));
+        alert(calcCentury(userYear) + 'век');
     } catch(ex) {
         alert('Что-то пошло не так, попробуйте еще раз!');
     }
